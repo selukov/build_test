3709effcd04117edeeb05f1d59d78d082b824562
andrew <selukov.andrey@gmail.com>
2025-04-16 00:19:31 +0700

    очистка json от комментариев

--- etc/ndm/netfilter.d/000-S-wave.sh
+++ etc/ndm/netfilter.d/000-S-wave.sh
@@ -3,17 +3,37 @@
 # Путь к JSON-файлу с настройками
 SETTING="/opt/etc/swave/settings.json"
 
+get_clean_json() {
+  awk '
+  BEGIN { in_string = 0 }
+  {
+    line = $0
+    result = ""
+    for (i = 1; i <= length(line); i++) {
+      char = substr(line, i, 1)
+      next_char = substr(line, i+1, 1)
+      if (char == "\"" && prev != "\\") {
+        in_string = !in_string
+      }
+      if (!in_string && char == "/" && next_char == "/") {
+        break
+      }
+      result = result char
+      prev = char
+    }
+    print result
+  }' "$1"
+}
+
+js_SETTING=$(get_clean_json "$SETTING" | jq -c '.' 2>/dev/null)
+
+
+echo "$js_SETTING"
+
+port_forwarding_list=$(echo "$js_SETTING" | jq -r '.network.port_forwarding_list | join(",")')
+
 # Извлекаем port_forwarding_list из JSON файла
-port_forwarding_list=$(jq -r '.network.port_forwarding_list | join(",")' $SETTING)
+#port_forwarding_list=$(jq -r '.network.port_forwarding_list | join(",")' $SETTING)
 
 # Выводим значение для проверки
 echo "Извлеченные порты: $port_forwarding_list"
-
-# Если вам нужен массив, можно преобразовать строку в массив
-IFS=',' read -ra ports_array <<< "$port_forwarding_list"
-
-# Пример использования массива
-echo "Доступные порты:"
-for port in "${ports_array[@]}"; do
-    echo "- $port"
-done
